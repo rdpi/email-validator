331ec01d8b4bded1ff3efc2e8b39ed25f2a3c78d
rdpi <ryanpitts95@gmail.com>
2017-03-02 10:48:06 -0400

    added two new rules

--- validator/src/main/java/com/validator/validator/Validator.java
+++ validator/src/main/java/com/validator/validator/Validator.java
@@ -7,9 +7,16 @@ public class Validator{
 		int rules = 0;
 		if (email.contains(".")){
 			++rules;
-			
 		}
 		if (email.matches("^[^@]*@[^@]*$")){
+			++rules;
+		}
+		//New rules: 1)String must be of form something@something.something
+		if (email.matches("^*@*.*$")){
+			++rules;
+		}
+		//2) TLD must only contain lower case letters
+		if (email.matches("^*.[a-z]$")){
 			++rules;
 		}
 		return rules;
